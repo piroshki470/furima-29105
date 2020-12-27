0901457cf817ba724023466d9040e9672e097485
piroshki470 <piroshki470@gmail.com>
2020-12-27 21:14:14 +0900

    出品機能実装(カテゴリー選択)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :ship_date
 
   with_options presence: true do
     validates :image
     validates :name
     validates :introduction
     validates :price
-    validates :category_id, numericality:{ other_than: 1 }
-    validates :status_id
-    validates :charge_id
-    validates :prefecture_id
-    validates :ship_date_id
+    validates :category_id,     numericality:{ other_than: 1 }
+    validates :status_id,       numericality:{ other_than: 1 }
+    validates :charge_id,       numericality:{ other_than: 1 }
+    validates :prefecture_id,   numericality:{ other_than: 1 }
+    validates :ship_date_id,    numericality:{ other_than: 1 }
   end
 end
