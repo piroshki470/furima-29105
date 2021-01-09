90dca50a663e2a51fa72a1c0fd008c0a05cbdd4a
piroshki470 <piroshki470@gmail.com>
2021-01-09 12:47:52 +0900

    レビュー修正4

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :introduction
-    validates :price,           numericality:{ only_integer: true, greater_than: 300, less_than: 10000000 }
+    validates :price,           numericality:{ only_integer: true, greater_than: 299, less_than: 10000000 }
     validates :category_id,     numericality:{ other_than: 1 }
     validates :status_id,       numericality:{ other_than: 1 }
     validates :charge_id,       numericality:{ other_than: 1 }
