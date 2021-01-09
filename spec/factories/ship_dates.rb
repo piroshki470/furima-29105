9700d69e55635ea30c0943e8b6f8f2a4d4d82ee9
piroshki470 <piroshki470@gmail.com>
2021-01-09 11:31:14 +0900

    レビュー修正2

--- spec/factories/ship_dates.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :ship_date do
-    
-  end
-end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,7 +38,7 @@ describe Item do
           expect(@item.errors.full_messages).to include("Category can't be blank")
         end
         it "カテゴリー情報が選択されていないとき" do
-          @item.category_id ="1"
+          @item.category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Category must be other than 1")
         end
@@ -48,7 +48,7 @@ describe Item do
           expect(@item.errors.full_messages).to include("Status can't be blank")
         end
         it "商品の状態が選択されていないとき" do
-          @item.status_id = "1"
+          @item.status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Status must be other than 1")
         end
@@ -58,7 +58,7 @@ describe Item do
           expect(@item.errors.full_messages).to include("Charge can't be blank")
         end
         it "配送料負担の情報が選択されていないとき" do
-          @item.charge_id = "1"
+          @item.charge_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Charge must be other than 1")
         end
@@ -68,7 +68,7 @@ describe Item do
           expect(@item.errors.full_messages).to include("Prefecture can't be blank")
         end
         it "発送元地域が選択されていないとき" do
-          @item.prefecture_id = "1"
+          @item.prefecture_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
         end
@@ -78,7 +78,7 @@ describe Item do
           expect(@item.errors.full_messages).to include("Ship date can't be blank")
         end
         it "発送までの日数情報が選択されていないとき" do
-          @item.ship_date_id = "1"
+          @item.ship_date_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Ship date must be other than 1")
         end
@@ -88,12 +88,12 @@ describe Item do
           expect(@item.errors.full_messages).to include("Price can't be blank")
         end
         it "価格が300円未満のとき" do
-          @item.price = "299"
+          @item.price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be greater than 300")
         end
         it "価格が9999999円を越えてるとき" do
-          @item.price = "10000000"
+          @item.price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be less than 10000000")
         end
