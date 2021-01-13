ca1818f0ddc451e0ef7522fbb91219a316ce5fff
piroshki470 <piroshki470@gmail.com>
2021-01-14 00:03:01 +0900

    Revert "Merge branch 'master' of https://github.com/piroshki470/furima-29105 into 商品詳細機能"
    This reverts commit 1cc016cf85e7eba9d3fbc8c0f183d4c4b58b024f, reversing
    changes made to cff03ff85b8008ae8b82c38964854cebabd2ed84.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,43 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show]
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+    render :new
+    end
+  end
+
+  def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+      redirect_to root_path
+  end
+
+  def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit( :name, :image, :introduction, :price, :category_id, :status_id, :charge_id, :prefecture_id, :ship_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+end
