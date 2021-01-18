22b8495081d4964f54404a7613a757bf216183ab
piroshki470 <piroshki470@gmail.com>
2021-01-18 13:12:43 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show,:edit]
+  before_action :set_item, only: [:show,:edit,:update]
 
   def index
     @items = Item.all
@@ -27,10 +27,8 @@ class ItemsController < ApplicationController
    end
 
    def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-    redirect_to item_path(@item.id)
+    if @item.update(item_params)
+       redirect_to item_path(@item.id)
     else
       render :edit
     end
